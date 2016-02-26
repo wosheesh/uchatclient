ae376921e5b5aef29cf96a16554a1046865f9249
wmaterka <w.materka@gmail.com>
2016-02-26 19:54:00 +0100

    moved

---
+++ uchat/Model/UdacityUser.swift
@@ -0,0 +1,35 @@
+//
+//  UdacityUser.swift
+//  On the Map
+//
+//  Created by Wojtek Materka on 29/01/2016.
+//  Copyright © 2016 Wojtek Materka. All rights reserved.
+//
+
+//TODO: Change the User model to accommodate ultiple users
+
+import Parse
+
+struct UdacityUser {
+    
+    // MARK: - Properties
+    static var udacityKey : String?
+    static var username: String?
+
+    // MARK: - Init
+    
+    // using First Name as the username to display in chat
+    init(initCurrentUserFromData userData: [String:AnyObject]) {
+        UdacityUser.udacityKey = userData[UClient.JSONResponseKeys.UserKey] as? String
+        UdacityUser.username = userData[UClient.JSONResponseKeys.FirstName] as? String
+    }
+    
+    static func clearData() {
+        UdacityUser.username = nil
+        UdacityUser.udacityKey = nil
+    }
+
+}
+    
+
+
